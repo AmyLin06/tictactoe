cda0498f086de2076476afc63766d2ceb31de9df
Amy <AmyLin1928@gmail.com>
2020-01-08 23:21:26 +0000

    created functions

--- js/script.js
+++ js/script.js
@@ -41,3 +41,50 @@ $("#button9").click(function() {
     performLogic("#button9","#tile9");
 });
 
+var player = "O"
+var totalTurn = 0
+var gameOver = false 
+
+function performLogic(buttonId, tileId){
+    if(!gameOver){
+        $(buttonId).hide();
+        $(timeId).html(player);
+        verticalWin();
+        horizontalWin();
+        diagonalWin();
+        changePLayer();
+        tieGame();
+    }
+}
+
+function changePLayer(){
+    totalTurn = totalTurn + 1
+    if(player == "O"){
+        player = "X"
+    }
+    else {
+        player = "O"
+    }
+}
+
+function verticalWin(){
+
+}
+
+function horizontalWin(){
+
+}
+
+function diagonalWin(){
+
+}
+
+function tieGame(){
+
+}
+
+function win(t1, t2, t3){
+    if(t1 == t2 == t3){
+        $(t1).html() == $(t2).html() 
+    }
+}
